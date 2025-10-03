0968a3bcc46c4d53b5f135f5891709b870f1c91b
Fabian <fabwpp@hotmail.com>
2025-10-02 21:45:42 -0500

    Add Unit Tests for Buy and Sell operations

---
+++ Cg.Tests/UnitTests/BuyOperationTests.cs
@@ -0,0 +1,60 @@
+﻿using Cg.Console;
+using Cg.Console.Models;
+using Cg.Console.Operations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cg.Tests.UnitTests
+{
+    [TestClass]
+    public class BuyOperationTests
+    {
+        [TestMethod]
+        public void Execute_Succeed()
+        {
+            var session = new TransactionSession()
+            {
+                Looses = 0,
+                Stock = 0,
+                WeightAvaragePrice = 0,
+            };
+
+            var operation = new BuyOperation(session);
+            var transaction = new Input(GeneralConfiguration.OPERATION_BUY, 100, 20);
+            var result = operation.Execute(transaction);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(0, result.Tax);
+            Assert.AreEqual(session.WeightAvaragePrice, transaction.UnitCost);
+            Assert.AreEqual(session.Stock, transaction.Quantity);
+        }
+
+        [TestMethod]
+        public void Execute_Succeed_CurrentStock()
+        {
+            decimal initialWAP = 50;
+            int initialStock = 20;
+            var session = new TransactionSession()
+            {
+                Looses = 0,
+                Stock = initialStock,
+                WeightAvaragePrice = initialWAP,
+            };
+
+            var operation = new BuyOperation(session);
+            var transaction = new Input(GeneralConfiguration.OPERATION_BUY, 100, 20);
+            var result = operation.Execute(transaction);
+
+            var expectedStock = initialStock + transaction.Quantity;
+            var expectedWAP = (initialWAP + transaction.UnitCost) / 2;
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(0, result.Tax);
+            Assert.AreEqual(expectedWAP, session.WeightAvaragePrice);
+            Assert.AreEqual(expectedStock, session.Stock);
+        }
+    }
+}
---
+++ Cg.Tests/UnitTests/SellOperationTests.cs
@@ -0,0 +1,181 @@
+﻿using Cg.Console;
+using Cg.Console.Exceptions;
+using Cg.Console.Models;
+using Cg.Console.Operations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cg.Tests.UnitTests
+{
+
+    [TestClass]
+    public class SellOperationTests
+    {
+        SellOperation operation;
+        TransactionSession session;
+
+        [TestMethod]
+        public void Execute_Succeed()
+        {
+            int initialStock = 300;
+            decimal initialWAP = 100;
+            session = new TransactionSession()
+            {
+                Stock = initialStock,
+                WeightAvaragePrice = initialWAP,
+                Looses = 0
+            };
+            Input transaction = new(GeneralConfiguration.OPERATION_SELL, 200, 300);
+            
+            decimal costOfStock = session.WeightAvaragePrice * transaction.Quantity;
+            decimal profitOfTransaction = (transaction.Quantity * transaction.UnitCost) - costOfStock;
+            decimal expectedTax = profitOfTransaction * (GeneralConfiguration.TAX/100);
+            int expectedStock = initialStock - transaction.Quantity;
+
+            operation = new SellOperation(session);
+            var taxResult = operation.Execute(transaction);
+
+            Assert.IsNotNull(taxResult);
+            Assert.AreEqual(expectedTax, taxResult.Tax);
+            Assert.AreEqual(expectedStock, session.Stock);
+            Assert.AreEqual(initialWAP, session.WeightAvaragePrice);
+        }
+
+        [TestMethod]
+        public void Execute_Succeed_UnderTaxExceptionLimit()
+        {
+            int initialStock = 300;
+            decimal initialWAP = 100;
+            session = new TransactionSession()
+            {
+                Stock = initialStock,
+                WeightAvaragePrice = initialWAP,
+                Looses = 0
+            };
+            Input transaction = new(GeneralConfiguration.OPERATION_SELL, 200, 50);
+
+            decimal costOfStock = session.WeightAvaragePrice * transaction.Quantity;
+            decimal profitOfTransaction = (transaction.Quantity * transaction.UnitCost) - costOfStock;
+            decimal expectedTax = profitOfTransaction * (GeneralConfiguration.TAX / 100);
+            int expectedStock = initialStock - transaction.Quantity;
+
+            operation = new SellOperation(session);
+            var taxResult = operation.Execute(transaction);
+
+            Assert.IsNotNull(taxResult);
+            Assert.AreEqual(0, taxResult.Tax);
+            Assert.AreEqual(expectedStock, session.Stock);
+            Assert.AreEqual(initialWAP, session.WeightAvaragePrice);
+        }
+
+        [TestMethod]
+        public void Execute_Succeed_LoosesProfit()
+        {
+            int initialStock = 300;
+            decimal initialWAP = 100;
+            session = new TransactionSession()
+            {
+                Stock = initialStock,
+                WeightAvaragePrice = initialWAP,
+                Looses = 0
+            };
+            Input transaction = new(GeneralConfiguration.OPERATION_SELL, 90, 300);
+
+            decimal costOfStock = session.WeightAvaragePrice * transaction.Quantity;
+            decimal profitOfTransaction = (transaction.Quantity * transaction.UnitCost) - costOfStock;
+            decimal expectedTax = 0;
+            int expectedStock = initialStock - transaction.Quantity;
+
+            operation = new SellOperation(session);
+            var taxResult = operation.Execute(transaction);
+
+            Assert.IsNotNull(taxResult);
+            Assert.AreEqual(expectedTax, taxResult.Tax);
+            Assert.AreEqual(expectedStock, session.Stock);
+            Assert.AreEqual(initialWAP, session.WeightAvaragePrice);
+            Assert.AreEqual(profitOfTransaction, session.Looses);
+        }
+
+        [TestMethod]
+        public void Execute_Succeed_DeductingLooses()
+        {
+            int initialStock = 300;
+            decimal initialWAP = 100;
+            session = new TransactionSession()
+            {
+                Stock = initialStock,
+                WeightAvaragePrice = initialWAP,
+                Looses = -20000
+            };
+            Input transaction = new(GeneralConfiguration.OPERATION_SELL, 200, 300);
+
+            decimal costOfStock = session.WeightAvaragePrice * transaction.Quantity;
+            decimal profitOfTransaction = (transaction.Quantity * transaction.UnitCost) - costOfStock;
+            decimal loosesDeducted = profitOfTransaction + session.Looses;
+            decimal expectedTax = loosesDeducted * (GeneralConfiguration.TAX / 100);
+            int expectedStock = initialStock - transaction.Quantity;
+
+            operation = new SellOperation(session);
+            var taxResult = operation.Execute(transaction);
+
+            Assert.IsNotNull(taxResult);
+            Assert.AreEqual(expectedTax, taxResult.Tax);
+            Assert.AreEqual(expectedStock, session.Stock);
+            Assert.AreEqual(initialWAP, session.WeightAvaragePrice);
+        }
+
+        [TestMethod]
+        public void Execute_Succeed_DeductingLooses_NoTaxes()
+        {
+            int initialStock = 300;
+            decimal initialWAP = 100;
+            session = new TransactionSession()
+            {
+                Stock = initialStock,
+                WeightAvaragePrice = initialWAP,
+                Looses = -80000
+            };
+            Input transaction = new(GeneralConfiguration.OPERATION_SELL, 200, 300);
+
+            decimal costOfStock = session.WeightAvaragePrice * transaction.Quantity;
+            decimal profitOfTransaction = (transaction.Quantity * transaction.UnitCost) - costOfStock;
+            decimal loosesDeducted = profitOfTransaction + session.Looses;
+            decimal expectedTax = loosesDeducted * (GeneralConfiguration.TAX / 100);
+            int expectedStock = initialStock - transaction.Quantity;
+
+            operation = new SellOperation(session);
+            var taxResult = operation.Execute(transaction);
+
+            Assert.IsNotNull(taxResult);
+            Assert.AreEqual(0, taxResult.Tax);
+            Assert.AreEqual(expectedStock, session.Stock);
+            Assert.AreEqual(initialWAP, session.WeightAvaragePrice);
+        }
+
+        [TestMethod]
+        public void Execute_Fail_OutOfStock()
+        {
+            int initialStock = 300;
+            decimal initialWAP = 100;
+            session = new TransactionSession()
+            {
+                Stock = initialStock,
+                WeightAvaragePrice = initialWAP,
+                Looses = 0
+            };
+            Input transaction = new(GeneralConfiguration.OPERATION_SELL, 200, 400);
+
+            decimal costOfStock = session.WeightAvaragePrice * transaction.Quantity;
+            decimal profitOfTransaction = (transaction.Quantity * transaction.UnitCost) - costOfStock;
+            decimal expectedTax = profitOfTransaction * (GeneralConfiguration.TAX / 100);
+            int expectedStock = initialStock - transaction.Quantity;
+
+            operation = new SellOperation(session);
+            Assert.ThrowsException<OutOfStockException>(()=> operation.Execute(transaction));
+        }
+    }
+}
