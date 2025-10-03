75bee1faa92f7d0d4e48330f0831250f6cb1f62a
Fabian <fabwpp@hotmail.com>
2025-10-02 19:08:50 -0500

    Add json property to Output model

--- Cg.Console/Models/Output.cs
+++ Cg.Console/Models/Output.cs
@@ -1,7 +1,17 @@
-﻿namespace Cg.Console.Models
+﻿using System.Text.Json.Serialization;
+
+namespace Cg.Console.Models
 {
-    public record Output(decimal Tax)
+    public record Output
     {
+        public Output(decimal tax)
+        {
+            Tax = tax;
+        }
+
+        [JsonPropertyName("tax")]
+        public decimal Tax { get; init; }
+
         public static Output Default => new(0);
     }
 }
